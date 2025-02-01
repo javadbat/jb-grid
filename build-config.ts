e9a6a1d8e4af5ac15a0a52dada00ec66657495dd
javadbat <javadbat@gmail.com>
2025-02-01 15:37:23 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,18 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-grid-react",
+    path: "./react/lib/JBGrid.tsx",
+    outputPath: "./react/dist/JBGrid.js",
+    external: ["mobx-react", "react", "mobx", "prop-types", "jb-searchbar"],
+    globals: {
+      react: "React",
+      mobx: "MobX",
+      "prop-types": "PropTypes",
+      "mobx-react": "mobxReact",
+      "jb-searchbar": "JBSearchbar"
+    },
+  },
+];
