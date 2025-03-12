33bac92085b9a03881e574952300748a0384a8d6
javadbat <javadbat@gmail.com>
2025-03-12 18:32:57 +0330

    chore: add storybook tests

---
+++ stories/JBGrid.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {JBGridData, JBGrid, Props} from 'jb-grid/react';
+import JBGridTest from './samples/JBGridTest.jsx';
+import CustomErrorTest from './samples/custom-error/JBGridTest.jsx';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<Props<any>> = {
+  title: "Example/JBGrid",
+  component: JBGrid,
+};
+export default meta;
+type Story = StoryObj<typeof JBGrid>;
+
+export const Normal:Story = {
+  render:(args) => <JBGridTest {...args}></JBGridTest>,
+  args:{
+    config: new JBGridData()
+  }
+};
+
+export const WithPersianNumber:Story = {
+  render:(args) => <JBGridTest {...args}></JBGridTest>,
+  args:{
+    config: new JBGridData(),
+    i18n:{showPersianNumber:true}
+  }
+};
+
+export const WithCustomError:Story = {
+  render:(args) => <CustomErrorTest {...args}></CustomErrorTest>,
+  args:{
+    config: new JBGridData()
+  }
+};
